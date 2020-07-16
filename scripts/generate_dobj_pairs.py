e74fafd927fde42b813db4bdcd8414efd81efc17
Bai Li <bai@cs.toronto.edu>
2020-07-16 18:12:49 -0400

    Add several ditransitive verbs, filter only ones that are semantically equivalent

--- scripts/generate_dobj_pairs.py
+++ scripts/generate_dobj_pairs.py
@@ -1,14 +1,21 @@
 import checklist
 from checklist.editor import Editor
 
+VERBS = ['gave', 'sent', 'mailed', 'took', 'brought', 'showed', 'sold']
+
 editor = Editor()
 
-ret = editor.template('The {mask} gave the {mask} a {mask}.')
-for sent in ret.data[:10]:
-  print(sent)
-
-print()
-
-ret = editor.template('The {mask} gave a {mask} to the {mask}.')
-for sent in ret.data[:10]:
-  print(sent)
+for vb in VERBS:
+  ret = editor.suggest(
+    f'The {{mask}} {vb} the {{mask}} a {{mask}}. The {{mask}} {vb} a {{mask}} to the {{mask}}.',
+    nsamples=1000 # Todo: this parameter seems to be broken with suggest() but works for template()
+  )
+  for t in ret:
+    if t[0] != t[3]: continue
+    if t[1] != t[5]: continue
+    if t[2] != t[4]: continue
+    if t[0] == t[1] or t[0] == t[2] or t[1] == t[2]: continue
+    subj = t[0]
+    iobj = t[1]
+    dobj = t[2]
+    print(f'The {subj} {vb} the {iobj} a {dobj}. The {subj} {vb} a {dobj} to the {iobj}.',)
