bed99abb55a3b82a72d5dbf8545dc9e214db216e
Bai Li <bai@cs.toronto.edu>
2020-08-03 18:12:24 -0400

    Fix: mean across token embeddings should ignore [PAD] tokens

--- tests/test_sent_encoder.py
+++ tests/test_sent_encoder.py
@@ -2,14 +2,16 @@ import unittest
 import src.sent_encoder
 import numpy as np
 
+EPS = 1e-9
+
 class TestSentEncoder(unittest.TestCase):
   def setUp(self):
     self.enc = src.sent_encoder.SentEncoder()
 
   def test_diff_encoder(self):
-    sent_pairs = [('I am a cat', 'I am a dog'), ('Life is good', 'Life is good')]
+    sent_pairs = [('I am a cat', 'I am not a cat'), ('Life is good', 'Life is good')]
     vecs = self.enc.evaluate_contextual_diff(sent_pairs)
 
     assert vecs.shape == (2, 768)
-    assert not np.all(vecs[0] == 0)
-    assert np.all(vecs[1] == 0)
+    assert np.sum(vecs[0]**2) > EPS
+    assert np.sum(vecs[1]**2) < EPS
