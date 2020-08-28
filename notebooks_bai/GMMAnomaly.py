c129c646f9c820df8406fd0993ac0f9fc69c1f5d
Bai Li <bai@cs.toronto.edu>
2020-08-28 00:41:40 -0400

    Refactor anomaly GMM into class

--- notebooks_bai/GMMAnomaly.py
+++ notebooks_bai/GMMAnomaly.py
@@ -10,30 +10,19 @@ import sys
 sys.path.append('../')
 
 import pickle
-from transformers import AutoTokenizer, AutoModelForMaskedLM
-import torch
 import random
-import sklearn.mixture
 
-import src.sent_encoder
 import src.sentpair_generator
+import src.anomaly_model
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
 
 
-# In[2]:
-
-
-model_name = 'roberta-base'
-bert_tokenizer = AutoTokenizer.from_pretrained(model_name)
-bert_model = AutoModelForMaskedLM.from_pretrained(model_name)
-
-
 # ## Pick random subset of sentences
 
-# In[3]:
+# In[2]:
 
 
 with open('../data/bnc.pkl', 'rb') as f:
@@ -43,92 +32,42 @@ random.seed(12345)
 bnc_sentences = random.sample(bnc_sentences, 1000)
 
 
-# ## Feed them through BERT
+# ## Train anomaly GMM
+
+# In[3]:
+
+
+anom = src.anomaly_model.AnomalyModel(bnc_sentences, layer=-2)
+
 
 # In[4]:
 
 
-enc = src.sent_encoder.SentEncoder()
+anom.gmm_score("The student laughs.", verbose=True)
 
 
 # In[5]:
 
 
-bnc_vecs = enc.contextual_token_vecs(bnc_sentences, layer=-2)
-
-
-# ## Train GMM
-
-# In[6]:
-
-
-gmm = sklearn.mixture.GaussianMixture()
-gmm.fit(bnc_vecs)
-
-
-# In[7]:
-
-
-def infer_new_sentence(sent):
-  ids = [x for x in enc.auto_tokenizer(sent)['input_ids'] if x not in enc.auto_tokenizer.all_special_ids]
-  sent_vecs = enc.contextual_token_vecs([sent])
-  assert len(ids) == sent_vecs.shape[0]
-  
-  for i in range(sent_vecs.shape[0]):
-    print(enc.auto_tokenizer.decode(ids[i]), gmm.score([sent_vecs[i]]))
-
-
-# In[8]:
-
-
-infer_new_sentence("The student laughs.")
-
-
-# In[9]:
-
-
-infer_new_sentence("The student laugh.")
+anom.gmm_score("The student laugh.", verbose=True)
 
 
 # ## Evaluate on Osterhout / Nicol data
 
-# In[10]:
+# In[6]:
 
 
 sentgen = src.sentpair_generator.SentPairGenerator()
 
 
-# In[11]:
+# In[7]:
 
 
-def gmm_score(sent):
-  ids = [x for x in enc.auto_tokenizer(sent)['input_ids'] if x not in enc.auto_tokenizer.all_special_ids]
-  sent_vecs = enc.contextual_token_vecs([sent])
-  assert len(ids) == sent_vecs.shape[0]
-  
-  score = 0
-  for i in range(sent_vecs.shape[0]):
-    score += gmm.score([sent_vecs[i]])
-  return score
-    
-    
-# Assume first of pair is the correct one
-def eval_sent_pairs(sentpairs):
-  got_right = 0
-  for correct_sent, incorrect_sent in sentpairs:
-    if gmm_score(correct_sent) > gmm_score(incorrect_sent):
-      got_right += 1
-  print(got_right / len(sentpairs))
+anom.eval_sent_pairs(sentgen.get_osterhout_nicol(anomaly_type='syntactic'))
 
 
-# In[12]:
+# In[8]:
 
 
-eval_sent_pairs(sentgen.get_osterhout_nicol(anomaly_type='syntactic'))
-
-
-# In[13]:
-
-
-eval_sent_pairs(sentgen.get_osterhout_nicol(anomaly_type='semantic'))
+anom.eval_sent_pairs(sentgen.get_osterhout_nicol(anomaly_type='semantic'))
 
