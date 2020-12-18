7f131f780016dd64faf4ae816d24bf4e67e5548f
Bai Li <bai@cs.toronto.edu>
2020-12-18 12:15:22 -0500

    Run plots for all layers

--- notebooks_bai/PositionAnomaly.py
+++ notebooks_bai/PositionAnomaly.py
@@ -77,49 +77,52 @@ for _, sent_pair_set in sentgen.get_hand_selected().items():
 
 # ## Compute anomaly difference, by position
 
-# In[7]:
+# In[29]:
 
-
-LAYER = 11
 
 df = []
 for s1, s2 in sent_pairs:
   tokens, all_layer = model.gmm_score([s1, s2])
   tok1 = tokens[0]
   tok2 = tokens[1]
-  anom1 = all_layer[0][LAYER]
-  anom2 = all_layer[1][LAYER]
-  
+  anom1 = all_layer[0]
+  anom2 = all_layer[1]
+
   # Exclude if they have different number of tokens
   if len(tok1) != len(tok2):
     continue
-  
+
   anom_diff = anom1 - anom2
-  
+
   # Compute position that they differ
   diff_pos = None
   for ix in range(len(tok1)):
     if tok1[ix] != tok2[ix]:
       diff_pos = ix
       break
-  
+
   # Add each token to df
   for ix in range(len(tok1)):
-    df.append(pd.Series({
-      'tok1': tok1[ix],
-      'tok2': tok2[ix],
-      'rel_position': ix - diff_pos,
-      'anom_diff': anom_diff[ix],
-    }))
+    for layer in range(13):
+      df.append(pd.Series({
+        'tok1': tok1[ix],
+        'tok2': tok2[ix],
+        'layer': layer,
+        'rel_position': ix - diff_pos,
+        'anom_diff': anom_diff[layer][ix],
+      }))
 
 df = pd.DataFrame(df)
 
 
-# ## Make plot
+# ## Make plot for each layer
 
-# In[12]:
+# In[30]:
 
 
-df_filtered = df[(df.rel_position >= -5) & (df.rel_position <= 5)]
-sns.barplot(x='rel_position', y='anom_diff', data=df_filtered, color='lightblue')
+for layer in range(13):
+  df_filtered = df[(df.rel_position >= -5) & (df.rel_position <= 5) & (df.layer == layer)]
+  sns.barplot(x='rel_position', y='anom_diff', data=df_filtered, color='lightblue')
+  plt.title(f'Layer: {layer}')
+  plt.show()
 
