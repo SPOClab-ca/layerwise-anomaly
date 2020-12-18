a8b7c6c40d1d9dd2cae3c5899f0ec8f35e172347
Bai Li <bai@cs.toronto.edu>
2020-12-18 12:01:12 -0500

    Relative position and anomaly diff barplot

--- notebooks_bai/PositionAnomaly.py
+++ notebooks_bai/PositionAnomaly.py
@@ -40,13 +40,13 @@ random.seed(12345)
 bnc_sentences = random.sample(bnc_sentences, 1000)
 
 
-# In[ ]:
+# In[3]:
 
 
 model = src.anomaly_model.AnomalyModel(bnc_sentences)
 
 
-# In[3]:
+# In[4]:
 
 
 sentgen = src.sentpair_generator.SentPairGenerator()
@@ -56,14 +56,14 @@ sentgen = src.sentpair_generator.SentPairGenerator()
 # 
 # Create a dataset consisting of max 100 sentences from each type, excluding pragmatic ones
 
-# In[10]:
+# In[5]:
 
 
 for task_name, sent_pair_set in sentgen.get_hand_selected().items():
   print(task_name, len(sent_pair_set.sent_pairs))
 
 
-# In[7]:
+# In[6]:
 
 
 sent_pairs = []
@@ -75,8 +75,51 @@ for _, sent_pair_set in sentgen.get_hand_selected().items():
     sent_pairs.extend(cur_sent_pairs)
 
 
-# In[11]:
+# ## Compute anomaly difference, by position
+
+# In[7]:
 
 
-print(len(sent_pairs))
+LAYER = 11
+
+df = []
+for s1, s2 in sent_pairs:
+  tokens, all_layer = model.gmm_score([s1, s2])
+  tok1 = tokens[0]
+  tok2 = tokens[1]
+  anom1 = all_layer[0][LAYER]
+  anom2 = all_layer[1][LAYER]
+  
+  # Exclude if they have different number of tokens
+  if len(tok1) != len(tok2):
+    continue
+  
+  anom_diff = anom1 - anom2
+  
+  # Compute position that they differ
+  diff_pos = None
+  for ix in range(len(tok1)):
+    if tok1[ix] != tok2[ix]:
+      diff_pos = ix
+      break
+  
+  # Add each token to df
+  for ix in range(len(tok1)):
+    df.append(pd.Series({
+      'tok1': tok1[ix],
+      'tok2': tok2[ix],
+      'rel_position': ix - diff_pos,
+      'anom_diff': anom_diff[ix],
+    }))
+
+df = pd.DataFrame(df)
+
+
+# ## Make plot
+
+# In[12]:
+
+
+df_filtered = df[(df.rel_position >= -5) & (df.rel_position <= 5)]
+sns.barplot(x='rel_position', y='anom_diff', data=df_filtered, color='lightblue')
 
